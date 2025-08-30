f031006f85464ca042bfd72fff0a9ebc4127e6f2
algorider111 <ahnaf.tahmid.web@gmail.com>
2025-08-30 17:14:57 +0600

    Update without_num_theory.cpp

--- without_num_theory.cpp
+++ without_num_theory.cpp
@@ -39,58 +39,6 @@
 
 using namespace std;
 
-typedef long long ll;
-typedef long double ld;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pll;
-// general macros
-#define endl '\n'
-#define sp ' '
-#define ff first
-#define ss second
-#define pb push_back
-#define ins insert
-#define all(x) (x).begin(), (x).end()
-#define rall(x) (x).rbegin(), (x).rend()
-#define sz(x) (int)(x).size()
-#define YES cout << "YES\n"
-#define NO cout << "NO\n"
-#define Yes cout << "Yes\n"
-#define No cout << "No\n"
-#define yO "YES\n"
-#define nO "NO\n"
-#define yo "Yes\n"
-#define no "No\n"
-#define MOD 1000000007
-#define mkp make_pair
-#define mems(a, x) memset((a), (x), sizeof(a))
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) ((a) * (b)) / gcd(a, b)
-#define sq(x) ((x) * (x))
-// vector and bs stuff
-#define MIN(v) *min_element(all(v))
-#define MAX(v) *max_element(all(v))
-#define LB(c, x) distance((c).begin(), lower_bound(all(c), (x)))
-#define UB(c, x) distance((c).begin(), upper_bound(all(c), (x)))
-#define UNIQUE(x) \
-  sort(all(x)), x.erase(unique(all(x)), x.end()), x.shrink_to_fit()
-// bits
-#define SET_BIT(n,k)       ((n) |  (1LL<<(k)))
-#define CLEAR_BIT(n,k)     ((n) & ~(1LL<<(k)))
-#define TOGGLE_BIT(n,k)    ((n) ^  (1LL<<(k)))
-#define TOGGLE_ALL_BITS(n) (~(n))
-#define TOGGLE_BIT_CHAR(c) ((c)=='0'?'1':'0')
-#define TOGGLE_ALL_BITS_STR(s) for(char &c:s) c=(c=='0'?'1':'0')
-#define CHECK_BIT(n,k)     (((n)>>(k)) & 1LL)
-#define LSB(x)             ((x) & -(x))
-#define MSB(x)             (63 - __builtin_clzll(x))
-#define BIT_COUNT(x)       (__builtin_popcountll(x))
-#define BIT_LENGTH(x)      (64 - __builtin_clzll(x))
-#define IS_POWER_OF_TWO(x) ((x) && !((x)&((x)-1)))
-#define ALL_BITS_SET(n)    ((1LL<<(n)) - 1)
-#define IS_SUBMASK(a,b)    (((a)&(b))==(a))
 // io stuff 
 template <typename T> void read(T &x){cin>>x;}
 template <typename T, typename... Args> void read(T &f, Args&... r){cin>>f; read(r...);}
