f6c507afeca16f3d8d8ecbc9fca16776b061da24
algorider111 <ahnaf.tahmid.web@gmail.com>
2025-08-30 18:32:22 +0600

    Update simple.cpp

--- simple.cpp
+++ simple.cpp
@@ -3,38 +3,40 @@
  | Author : Algorider                                          |
  --------------------------------------------------------------*/
 
-#define tc_need
-// #define i_want_bug_free_code
+#define tc_need                        // enable test case loop
+#define all(x) (x).begin(), (x).end()  // shorthand for containers
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)x.size()
 
-#ifdef i_want_bug_free_code
-#include "algorider/debug.h"  // Algorider Debug
-#else
-#define debug(...) ((void)0)   
-#define deb(x) ((void)0)      
-#endif
-
-#include<iostream>
-#include<vector>
-#include<algorithm>
-
+#include <iostream>
+#include <vector>       // vectors
+#include <algorithm>    // sort, min, max, reverse
 using namespace std;
 
+typedef long long ll;
 
-// my solution
-void Solve() { 
-   
+#ifdef i_want_bug_free_code
+#include "algorider/debug.h"  
+#else
+#define debug(...) ((void)0)
+#define deb(x) ((void)0)
+#endif
+
+void Solve() {
+    // your solution goes here
 }
 
 int main() {
-  ios::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
 #ifdef tc_need
-  int t;
-  read(t);
-  while (t--) Solve();
+    int tc;
+    cin >> tc;
+    while (tc--) Solve();
 #else
-  Solve()
+    Solve();
 #endif
-  return 0;
+
+    return 0;
 }
