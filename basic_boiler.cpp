b84bf363db219de572769e7ae8750af176af14f7
algorider111 <ahnaf.tahmid.web@gmail.com>
2025-10-04 11:32:00 +0600

    Create basic_boiler.cpp

---
+++ basic_boiler.cpp
@@ -0,0 +1,32 @@
+//---------------------------------------------
+// In the Name of Allah, the Most Gracious, the Most Merciful
+// Author : Algorider
+//---------------------------------------------
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+typedef long long ll;
+
+void Solve(void) { 
+  // Solution goes here
+}
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+
+#ifndef ONLINE_JUDGE
+  freopen("in.txt", "r", stdin);
+  freopen("out.txt", "w", stdout);
+  freopen("err.txt", "w", stderr);
+#endif
+
+#ifdef TC
+  for (int tc = (cin >> tc, tc); tc--;) Solve();
+#else
+  Solve();
+#endif
+
+  return 0;
+}
