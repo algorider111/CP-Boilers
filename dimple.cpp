60c0466c6103a3834b415b23c18ecbc0a006ca26
algorider111 <ahnaf.tahmid.web@gmail.com>
2025-10-08 16:27:17 +0600

    Create dimple.cpp

---
+++ dimple.cpp
@@ -0,0 +1,37 @@
+//---------------------------------------------
+// In the Name of Allah, the Most Gracious, the Most Merciful
+// Author : Algorider
+//---------------------------------------------
+
+// C:\Users\Ahnaf
+
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("Ofast,unroll-loops,fast-math,O3")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+typedef long long ll;
+
+
+
+// #define i_want_bug_free_code // debug 
+#ifdef i_want_bug_free_code
+#include "algorider/debug.h"
+#else
+#define debug(...) ((void)0)
+#define deb(x) ((void)0)
+#endif
+
+#define sz(x) (int)x.size()
+
+void Solve(void) { 
+  // Write your logic here
+}
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  int tc; cin >> tc;
+  while(tc--) KALARRY();
+  return 0;
+}
